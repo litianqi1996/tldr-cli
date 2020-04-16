bf234df094dc72e0f6929de847ef5a202edc6520
litianqi <litianqi1995@gmail.com>
2020-04-16 16:31:27 +0800

    format show pages

--- cmd/show.go
+++ cmd/show.go
@@ -13,38 +13,9 @@ var (
 	titleShow       = color.New(color.FgWhite, color.Bold).PrintlnFunc()
 	referShow       = color.New(color.FgWhite).PrintlnFunc()
 	instructionShow = color.New(color.FgGreen).PrintlnFunc()
-	commandShow     = color.New(color.FgCyan).PrintFunc()
+	commandShow     = color.New(color.FgCyan).PrintFunc() //not println
 	argsShow        = color.New(color.FgRed).PrintFunc()
 )
-
-func ShowItem(command string) {
-	items := strings.Split(strings.Trim(command, "`"), " ")
-	for _, item := range items {
-		if strings.Contains(item, "{{") {
-			commandShow(" " + strings.Trim(item, "{{}}") + " ") //todo: bug in head and tail
-		} else {
-			argsShow(" " + item)
-		}
-	}
-}
-
-func Show(pages []byte) {
-
-	lhs := strings.Split(string(pages), "\n")
-	for _, value := range lhs {
-		switch {
-		case strings.HasPrefix(value, "-"):
-			instructionShow(value)
-		case strings.HasPrefix(value, "`"):
-			ShowItem(value)
-			fmt.Println("\n")
-		case strings.HasPrefix(value, "#"):
-			titleShow(strings.Split(value, "#")[1] + "\n")
-		case strings.HasPrefix(value, ">"):
-			referShow(strings.Split(value, ">")[1] + "\n")
-		}
-	}
-}
 
 func Getpage(cmd string) error {
 	cmdpath, err := SearchPages(cmd, RepoPath, Language)
@@ -55,6 +26,50 @@ func Getpage(cmd string) error {
 	if err != nil {
 		return err
 	}
-	Show(page)
+	Show(string(page))
 	return nil
+}
+
+func Show(pages string) {
+
+	fs := format(pages)
+
+	for _, value := range fs {
+		switch {
+		case strings.HasPrefix(value, "-"):
+			instructionShow(value)
+		case strings.HasPrefix(value, "`"):
+			showItem(value)
+		case strings.HasPrefix(value, "#"):
+			titleShow("\n" + strings.TrimLeft(value, "#"))
+		case strings.HasPrefix(value, ">"):
+			referShow(strings.TrimLeft(value, ">"))
+		case value == "":
+			fmt.Print("\n")
+		}
+	}
+}
+
+func showItem(command string) {
+	items := strings.Split(strings.Trim(command, "`"), " ")
+	for _, item := range items {
+		if strings.Contains(item, "{{") {
+			commandShow(" " + strings.ReplaceAll(strings.ReplaceAll(item, "{{", ""), "}}", "") + " ")
+		} else {
+			argsShow(" " + item)
+		}
+	}
+	fmt.Print("\n")
+}
+
+func format(page string) []string {
+	lhs := strings.Split(string(page), "\n")
+	for i, value := range lhs {
+		if strings.HasPrefix(value, "-") {
+			if lhs[i+1] == "" {
+				lhs = append(lhs[:i+1], lhs[i+2:]...)
+			}
+		}
+	}
+	return lhs
 }
